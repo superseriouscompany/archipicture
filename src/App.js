1d3980482f0fee5754bd5bc81fbd580e57335af1
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-02-26 20:35:25 +0000

    Put a box in the center

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import StripeCheckout from 'react-stripe-checkout'
-
-const baseUrl = window.location.href.match(/localhost/) ?
-  'http://localhost:3000' :
-  'https://archipicture.superserious.co';
+import Hello from './Hello';
+import Home from './Home';
 
 export default class App extends Component {
-  onToken(token) {
-    const body = {
-      email: token.email,
-      token: token.id,
-    }
-    fetch(`${baseUrl}/pay`, {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers: { 'Content-Type': 'application/json' },
-    }).then((response) => {
-      if( !response.ok ) {
-        throw(new Error('nope'));
-      }
-      alert('Paid.')
-    }).catch((err) => {
-      alert(err);
-    })
-    console.log(token);
-  }
-
   render() { return (
-    <div className="hello">
-      <a href="https://github.com/login/oauth/authorize?scope=user:email&client_id=a3babf538c0e87a2d6ee">Login with Github</a>
-      <a href="https://instagram.com/superseriouscompany" target="_blank">Instagram</a>
-      <StripeCheckout
-        token={this.onToken}
-        stripeKey="pk_test_mmQIM1nxdtJu1AymtDN9nBCo"
-        name="Super Serious Company"
-        description="Help."
-        panelLabel="Lose"
-        amount={999}>
-        <button className="pay">Donate</button>
-      </StripeCheckout>
-    </div>
+    <Home />
   )}
 }
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Home.css'
+
+export default class Home extends Component {
+  render() { return (
+    <div className="diagram">
+      <hr width="1" size="100"/>
+      <div className="create box">
+        +
+      </div>
+    </div>
+  )}
+}
