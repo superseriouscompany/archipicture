6bb5c871434a3845ad08d89fa8eb2c358e5a1662
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-02-27 17:01:22 +0000

    Checkpoint: allow creating instances

--- src/components/Google.js
+++ src/components/Google.js
@@ -17,6 +17,8 @@ export default class Google extends Component {
       <a onClick={this.login.bind(this)}>Login</a>
       <br /><br />
       <a onClick={this.loadInstances.bind(this)}>Load instances</a>
+      <br /><br />
+      <a onClick={this.createInstance.bind(this)}>Create instance</a>
       { this.state.instances.map((i, key) => (
         <dl key={key}>
           <dt>id</dt>
@@ -32,7 +34,7 @@ export default class Google extends Component {
           <dt>size</dt>
           <dd>{i.machineType.split('/').slice(-1)[0]}</dd>
           <dt>pubkey</dt>
-          <dd>{i.metadata.items[0].value}</dd>
+          <dd>{i.metadata.items && i.metadata.items[0] && i.metadata.items.value}</dd>
           <dt>status</dt>
           <dd>{i.status}</dd>
         </dl>
@@ -51,6 +53,47 @@ export default class Google extends Component {
         instances: resp.items
       })
       console.log(resp)
+    })
+  }
+
+  createInstance() {
+    const resource = {
+      name:        'instance-3',
+      zone:        'projects/archipicture-1/zones/europe-west1-c',
+      machineType: 'projects/archipicture-1/zones/europe-west1-c/machineTypes/f1-micro',
+      "disks": [{
+        "type": "PERSISTENT",
+        "boot": true,
+        "mode": "READ_WRITE",
+        "autoDelete": true,
+        "deviceName": "instance-3",
+        "initializeParams": {
+          "sourceImage": "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1604-xenial-v20170224",
+          "diskType": "projects/archipicture-1/zones/europe-west1-c/diskTypes/pd-ssd",
+          "diskSizeGb": "10"
+        }
+      }],
+      "networkInterfaces": [{
+        "network": "projects/archipicture-1/global/networks/default",
+        "subnetwork": "projects/archipicture-1/regions/europe-west1/subnetworks/default",
+        "accessConfigs": [
+          {
+            "name": "External NAT",
+            "type": "ONE_TO_ONE_NAT"
+          }
+        ]
+      }],
+    }
+
+    const request = window.gapi.client.compute.instances.insert({
+      project: 'archipicture-1',
+      zone:    'europe-west1-c',
+      resource: resource,
+    })
+
+    request.execute((resp) => {
+      if( resp.code ) { console.error(resp); return alert(JSON.stringify(resp))}
+      console.log(resp);
     })
   }
 
